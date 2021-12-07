2a67b630966e5c1ba197353a698350aaea5afefc
Alvar <alvararias@gmail.com>
2021-12-07 10:05:01 +0100

    Initial Commit

---
+++ playerSwiftUI/playerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  playerSwiftUIApp.swift
+//  playerSwiftUI
+//
+//  Created by Alvar Arias on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct playerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
