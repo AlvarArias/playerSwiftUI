96375b60e207821e092ab2674bc5cc828901460d
Alvar Arias <alvararias@gmail.com>
2022-12-17 13:35:13 +0100

    App ready for testing

--- playerSwiftUI/Favorites/FavoriteDispView.swift
+++ playerSwiftUI/Favorites/FavoriteDispView.swift
@@ -92,7 +92,10 @@ struct FavoriteDispView: View {
 
                     }
            }
+                .navigationBarTitle("Favorites", displayMode: .inline)
+            
         }
+        
     }
            
     
@@ -101,9 +104,7 @@ struct FavoriteDispView: View {
         if let savedPerson = defaults.object(forKey: "SavedPerson") as? Data {
             let decoder = JSONDecoder()
             if let loadedPerson = try? decoder.decode(Person.self, from: savedPerson) {
-                //print("loadedPerson.name")
-                print(loadedPerson.mytest)
-                
+               
                 // Check value in array
                 if loadedPerson.mytest.contains(where: {$0 == myRadioFavo}) {
                    // it exists, do something
