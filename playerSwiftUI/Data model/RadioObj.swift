c6d2f1d61deeb4469cc375b1c6bea2b444fec015
Alvar Arias <alvararias@gmail.com>
2022-12-18 14:48:18 +0100

    new version for IOS 13

--- playerSwiftUI/Data model/RadioObj.swift
+++ playerSwiftUI/Data model/RadioObj.swift
@@ -20,64 +20,6 @@ import UIKit
 
 import Foundation
 
-// MARK: - MyRadioGen
-struct MyRadioGen {
-    let image, imagetemplate: String
-    let color, tagline: String
-    let siteurl: String
-    let url: String
-    let scheduleurl: String
-    let channeltype: Channeltype
-    let xmltvid: String
-}
-
-
-enum Channeltype {
-    case lokalKanal
-    case rikskanal
-}
-
-
-
-struct myRadio {
-    let radioURL : String
-    let chanelID : String
-    let radioName : String
-    let radioLogoName : String
-}
-
-/*
- //let url2  = URL.init(string:"https://sverigesradio.se/topsy/direkt/srapi/2562.mp3")
-<channel id="213" name="P4 Blekinge">
-<channel id="223" name="P4 Dalarna">
-<channel id="205" name="P4 Gotland">
-
-
- <channel id="132" name="P1">
- https://api.sr.se/api/v2/channels/132
- http://sverigesradio.se/topsy/direkt/srapi/132.mp3
-
- <channel id="163" name="P2">
- https://api.sr.se/api/v2/channels/163
- https://sverigesradio.se/topsy/direkt/srapi/2562.mp3
-
- <channel id="164" name="P3">
- https://api.sr.se/api/v2/channels/164
- https://sverigesradio.se/topsy/direkt/srapi/164.mp3
- 
- */
-
-// test variable que pasa data del home to detail
-class User: ObservableObject {
-    @Published var score = 0
-}
-
-class User2: ObservableObject {
-    @Published var image = "url test"
-    @Published var imagetemplate: String?
-    @Published var scheduleurl: String?
-}
-
 // Listado de radios 
 struct DemoRadio: Hashable, Codable  {
 
@@ -95,17 +37,3 @@ struct DemoRadio: Hashable, Codable  {
     
     
 }
-
-class Favorites: ObservableObject {
-    @Published var theRadios: Set<DemoRadio> = []
-    //@Published var favorites: Set<String> = []
-    /**
-     init(red: Double, green: Double, blue: Double) {
-             self.red   = red
-             self.green = green
-             self.blue  = blue
-         }
-     */
-    
-}
-
---
+++ playerSwiftUI2/playerSwiftUI2App.swift
@@ -0,0 +1,18 @@
+//
+//  playerSwiftUI2App.swift
+//  playerSwiftUI2
+//
+//  Created by Alvar Arias on 2022-12-18.
+//
+
+import SwiftUI
+@available(iOS 14.0, *)
+@main
+struct playerSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+            SliderSwiftUIView()
+        }
+    }
+}
