bf3f8b513bb6cf1fa6b9c7624007b7b5e4b38402
Alvar Arias <alvararias@gmail.com>
2022-12-18 14:58:15 +0100

    basic version pro streaming test

--- playerSwiftUI2/Detail View/DetalleUIView.swift
+++ playerSwiftUI2/Detail View/DetalleUIView.swift
@@ -6,14 +6,89 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct DetalleUIView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    
     @State var choice : String
     @State var choice1 : DemoRadio
+    @State var isPlaying = false
+    
+    @State var player = AVPlayer()  
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+        
+        
+        Button( action: {
+            
+            player = AVPlayer(url: URL(string: choice1.url)!)
+            self.isPlaying.toggle()
+            player.play()
+            //isShowEq = true
+        
+            if isPlaying == false {
+                player.pause()
+                //isShowEq = false
+            }
+            
+            //TODO: Check backgroung sound is working and how to stop it See add Wiget for playing control
+            // Function play in background
+            
+            /*
+            do {
+                try AVAudioSession.sharedInstance()
+                                      .setCategory(AVAudioSession.Category.playback)
+                print("AVAudioSession Category Playback OK")
+                do {
+                    try AVAudioSession.sharedInstance().setActive(true)
+                    print("AVAudioSession is Active")
+                } catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            */
+    
+        })
+        {
+            if isPlaying == true {
+                Image("Pause2").resizable().aspectRatio(contentMode: .fit).frame(width: 100, height: 100)
+                
+            } else {
+                Image("But-Play2").resizable().aspectRatio(contentMode: .fit).frame(width: 100, height: 100)
+            }
+        }
+            
+        }.navigationBarTitle("Radio \(choice1.name)", displayMode: .inline)
+            // new button back
+        
+            .navigationBarItems(
+                leading:
+                   // NavigationLink("Go to back", destination: SliderSwiftUIView())
+                
+                Button(action : {
+                    
+                    player.pause()
+                    self.presentationMode.wrappedValue.dismiss()
+                    print("click and stop audio")
+                }){
+                        //Text("Back").foregroundColor(.newSecundaryColor)
+                    Image(systemName: "arrow.uturn.backward")
+                        .foregroundColor(.newSecundaryColor)
+                    
+                    
+                }
+            
+            ).background(Color.newColorGreenLight)
+    
+            .navigationBarBackButtonHidden(true)
+    
+
     }
 }
 
