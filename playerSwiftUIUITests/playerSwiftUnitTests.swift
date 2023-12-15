872a267635f0914c83b2740eeee99eb2fadc7fc6
Alvar Arias <alvararias@gmail.com>
2023-12-15 17:13:45 +0100

    last changes

--- playerSwiftUIUITests/playerSwiftUnitTests.swift
+++ playerSwiftUIUITests/playerSwiftUnitTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class playerSwiftUIUITestsLaunchTests: XCTestCase {
+class playerSwiftUnitTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
